9cd94addb0cf95ff826caf2506a8bfc55d7ec83c
Dio <1sl1.1dio1@gmail.com>
2017-08-21 19:42:23 +0300

    add abstract

---
+++ src/main/java/enetety/Human.java
@@ -0,0 +1,14 @@
+package enetety;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by 1 on 21.08.2017.
+ */
+public abstract class Human
+{
+    String firstName;
+    String serName;
+    Timestamp birthDate;
+    String position;
+}
---
+++ src/main/java/enetety/People.java
@@ -0,0 +1,8 @@
+package enetety;
+
+/**
+ * Created by 1 on 21.08.2017.
+ */
+public class People extends Human
+{
+}
